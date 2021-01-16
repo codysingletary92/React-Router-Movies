a6527bc954903e428c83085512da677bde38ab55
samuel singletary <codysingletary92@gmail.com>
2021-01-16 15:24:31 -0500

    Late, but finished

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import Movie from './Movie';
+import {useHistory} from 'react-router-dom';
 
 export default function MovieList(props) {
+  
+  
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {props.props.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
 }
 
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
 
+
+
+function MovieDetails(props) {
+  const { title, director, metascore, id } = props.movie;
+  const history = useHistory();
+
+  function goToMovie(){
+    history.push('/Movies/'+id)
+  }
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={goToMovie}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
